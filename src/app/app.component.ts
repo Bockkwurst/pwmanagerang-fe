a88a537a3c4b935dba6567df2dd5b1211414efb2
Bockkwurst <kremer.1991@web.de>
2024-06-18 22:51:26 +0200

    added links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import {InfoboxComponent} from "./infobox/infobox.component";
 import {DefaultButtonComponent} from "./default-button/default-button.component";
 import {LandingHeadComponent} from "./landing-head/landing-head.component";
+import {NavbarComponent} from "./navbar/navbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, InfoboxComponent, DefaultButtonComponent, LandingHeadComponent],
+  imports: [RouterOutlet, InfoboxComponent, DefaultButtonComponent, LandingHeadComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import {LandingHeadComponent} from "./landing-head/landing-head.component";
+import {ContactComponent} from "./contact/contact.component";
+import {RegisterComponent} from "./register/register.component";
+import {LoginComponent} from "./login/login.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: LandingHeadComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'contact', component: ContactComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
